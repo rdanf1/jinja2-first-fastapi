2e42bdaa95958337385fb89b33f743f914514132
jaybon1 <ponggas2@gmail.com>
2022-12-03 16:40:12 +0900

    post 통신 추가

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 
 import uvicorn
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi import FastAPI, Request
 
 # 실행경로가 다르더라도 정확한 주소를 받기 위해 사용
 main_dir = os.path.dirname(__file__)
@@ -19,6 +20,12 @@ app.mount(
 @app.get("/")
 async def test(request: Request):
     return templates.TemplateResponse("test.html", {"request": request, "data": "테스트"})
+
+
+@app.post("/result")
+async def test(idx: int = Form()):
+    return {"idx": idx}
+
 
 # 패키지 경로를 정확히 하기 위해서 아래방식으로 실행
 if __name__ == "__main__":
